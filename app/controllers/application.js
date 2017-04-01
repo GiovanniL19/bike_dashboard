5a01753f96883c52316c313c2a92d599ee3c8d98
Giovanni Lenguito <giovanni16.gl@gmail.com>
2017-04-01 10:20:38 +0100

    - Implements line graph

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -6,6 +6,40 @@ export default Ember.Controller.extend({
   missingItems: 0,
   completeItems: 0,
 
+  chartData: null,
+
+  addData: function(){
+    var data = [this.get("processingItems"), this.get("missingItems"), this.get("completeItems")];
+    var chartData = {
+      labels: ["Processing", "Missing Items", "Complete"],
+        datasets: [
+        {
+          label: "Orders",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "rgba(75,192,192,1)",
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: data,
+          spanGaps: false,
+        }
+      ]
+    };
+    this.set("chartData", chartData);
+  }.observes("processingItems", "missingItems", "completeItems"),
+
   processingItemsObserver: function(){
     let controller = this;
 
